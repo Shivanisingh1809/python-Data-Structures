378efdd6bc8b1cae1da70777b59c39448edc8093
Shivani Kumari <60873464+Shivanisingh1809@users.noreply.github.com>
2022-10-08 23:39:24 +0530

    Add files via upload
    Implementation of tuple using python

---
+++ tuple in python.py
@@ -0,0 +1,31 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# Creating a Tuple with
+# the use of Strings
+Tuple = ('python', 'fab')
+print("\nTuple with the use of String: ")
+print(Tuple)
+      
+# Creating a Tuple with
+# the use of list
+list1 = [1, 8, 0, 9, 6]
+print("\nTuple using List: ")
+Tuple = tuple(list1)
+  
+# Accessing element using indexing
+print("First element of tuple")
+print(Tuple[0])
+  
+# Accessing element from last
+# negative indexing
+print("\nLast element of tuple")
+print(Tuple[-1])
+  
+print("\nThird last element of tuple")
+print(Tuple[-3])
