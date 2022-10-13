920406fbce7e8773e18d196927409c1928e1085b
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2022-10-14 00:24:57 +0530

    fix: added stack and queue implementation

---
+++ queue.py
@@ -0,0 +1,18 @@
+queue = []
+
+# Adding elements to the queue
+queue.append('g')
+queue.append('f')
+queue.append('g')
+
+print("Initial queue")
+print(queue)
+
+# Removing elements from the queue
+print("\nElements dequeued from queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)
